f4a67d21d52280fa4871f78e1817f3a254312956
Linsn <linsn@sd-53233.dedibox.fr>
2021-02-02 11:13:27 +0100

    update update-all.sh script

--- update-all.sh
+++ update-all.sh
@@ -7,7 +7,7 @@ echo "${HTTP_USER}:${HTTP_PASSWORD}" > traefik/http_auth
 echo "[$0] ***** Pulling all images... *****"
 docker-compose pull
 echo "[$0] ***** Recreating containers if required... *****"
-docker-compose up -d --remove-orphans
+docker-compose -f docker-compose-traefik.yml -f docker-compose.yml up -d --remove-orphans
 echo "[$0] ***** Done updating containers *****"
 echo "[$0] ***** Clean unused images... *****"
 docker image prune -af
