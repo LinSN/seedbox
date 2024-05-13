96dd6b7576e42e0e69def5f719b749c6d5d86371
LinSN <slimaninordine@gmail.com>
2024-05-14 00:01:24 +0200

    update for cron task

--- update-all.sh
+++ update-all.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 # Create/update http_auth file according to values in .env file
-source .env
-echo "${HTTP_USER}:${HTTP_PASSWORD}" > traefik/http_auth
+set -a
+source /home/linsn/seedbox/.env
+set +a
+
+# Use the environment variables to create the http_auth file
+echo "${HTTP_USER}:${HTTP_PASSWORD}" > /home/linsn/seedbox/traefik/http_auth
+
+# source /home/linsn/seedbox/.env
+# echo "${HTTP_USER}:${HTTP_PASSWORD}" > traefik/http_auth
 
 echo "[$0] ***** Pulling all images... *****"
-docker-compose pull
+/usr/local/bin/docker-compose -f /home/linsn/seedbox/docker-compose-traefik.yml -f /home/linsn/seedbox/docker-compose.yml pull
 echo "[$0] ***** Recreating containers if required... *****"
-docker-compose -f docker-compose-traefik.yml -f docker-compose.yml up -d --remove-orphans
+/usr/local/bin/docker-compose -f /home/linsn/seedbox/docker-compose-traefik.yml -f /home/linsn/seedbox/docker-compose.yml up -d --remove-orphans
 echo "[$0] ***** Done updating containers *****"
 echo "[$0] ***** Clean unused images... *****"
 docker image prune -af
