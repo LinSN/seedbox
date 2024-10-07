05fe9a240f05a658eb5feb940e73060e4a8fee8c
LinSN <slimaninordine@gmail.com>
2024-10-07 15:17:58 +0200

    udpate dockercompose

--- update-all.sh
+++ update-all.sh
@@ -12,9 +12,9 @@ echo "${HTTP_USER}:${HTTP_PASSWORD}" > /home/linsn/seedbox/traefik/http_auth
 # echo "${HTTP_USER}:${HTTP_PASSWORD}" > traefik/http_auth
 
 echo "[$0] ***** Pulling all images... *****"
-/usr/local/bin/docker-compose -f /home/linsn/seedbox/docker-compose-traefik.yml -f /home/linsn/seedbox/docker-compose.yml pull
+/usr/local/bin/docker-compose pull
 echo "[$0] ***** Recreating containers if required... *****"
-/usr/local/bin/docker-compose -f /home/linsn/seedbox/docker-compose-traefik.yml -f /home/linsn/seedbox/docker-compose.yml up -d --remove-orphans
+/usr/local/bin/docker-compose up -d --remove-orphans
 echo "[$0] ***** Done updating containers *****"
 echo "[$0] ***** Clean unused images... *****"
 docker image prune -af
