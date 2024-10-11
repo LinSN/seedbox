c73c42548a3e2f6cf061c30e4c2940d6f73f04a2
LinSN <slimaninordine@gmail.com>
2024-10-11 11:07:39 +0200

    working google auth

--- update-all.sh
+++ update-all.sh
@@ -6,17 +6,17 @@ source /home/linsn/seedbox/.env
 set +a
 
 # Use the environment variables to create the http_auth file
-echo "${HTTP_USER}:${HTTP_PASSWORD}" > /home/linsn/seedbox/traefik/http_auth
+# echo "${HTTP_USER}:${HTTP_PASSWORD}" > /home/linsn/seedbox/traefik/http_auth
 
 # source /home/linsn/seedbox/.env
 # echo "${HTTP_USER}:${HTTP_PASSWORD}" > traefik/http_auth
 
 echo "[$0] ***** Pulling all images... *****"
-/usr/local/bin/docker-compose pull
+# /usr/local/bin/docker-compose pull
 echo "[$0] ***** Recreating containers if required... *****"
 /usr/local/bin/docker-compose up -d --remove-orphans
 echo "[$0] ***** Done updating containers *****"
 echo "[$0] ***** Clean unused images... *****"
-docker image prune -af
+# docker image prune -af
 echo "[$0] ***** Done! *****"
 exit 0
