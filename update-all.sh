1c7669bcf8a5549d3f859f0a0f63a81202d9a4b0
LinSN <slimaninordine@gmail.com>
2024-10-14 11:50:18 +0200

    update update-all.sh

--- update-all.sh
+++ update-all.sh
@@ -12,11 +12,11 @@ set +a
 # echo "${HTTP_USER}:${HTTP_PASSWORD}" > traefik/http_auth
 
 echo "[$0] ***** Pulling all images... *****"
-# docker compose pull
+docker compose pull
 echo "[$0] ***** Recreating containers if required... *****"
 docker compose up -d --remove-orphans
 echo "[$0] ***** Done updating containers *****"
 echo "[$0] ***** Clean unused images... *****"
-# docker image prune -af
+docker image prune -af
 echo "[$0] ***** Done! *****"
 exit 0
