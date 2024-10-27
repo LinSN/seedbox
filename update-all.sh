6ccbc0d242b5317150ac5a78627319191a097b2d
LinSN <slimaninordine@gmail.com>
2024-10-27 11:00:57 +0100

    Create a better log system for update-all.sh script

--- update-all.sh
+++ update-all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Create/update http_auth file according to values in .env file
-set -a
-source /home/linsn/seedbox/.env
-set +a
+# set -a
+# source /home/linsn/seedbox/.env
+# set +a
 
 # Use the environment variables to create the http_auth file
 # echo "${HTTP_USER}:${HTTP_PASSWORD}" > /home/linsn/seedbox/traefik/http_auth
@@ -11,12 +11,28 @@ set +a
 # source /home/linsn/seedbox/.env
 # echo "${HTTP_USER}:${HTTP_PASSWORD}" > traefik/http_auth
 
-echo "[$0] ***** Pulling all images... *****"
-docker compose pull
-echo "[$0] ***** Recreating containers if required... *****"
-docker compose up -d --remove-orphans
-echo "[$0] ***** Done updating containers *****"
-echo "[$0] ***** Clean unused images... *****"
+# Change to the seedbox directory
+# Define the log file path
+log_file="/home/linsn/logs/update-all.log"
+
+# Define a logging function
+log() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$log_file"
+}
+
+# Change to the seedbox directory
+cd /home/linsn/seedbox
+
+log "***** Starting update-all.sh script *****"
+
+log "Pulling all images..."
+docker compose -f docker-compose.yml --env-file .env pull
+
+log "Recreating containers if required..."
+docker compose -f docker-compose.yml --env-file .env up -d --remove-orphans
+
+log "Cleaning unused images..."
 docker image prune -af
-echo "[$0] ***** Done! *****"
+
+log "***** Finished update-all.sh script *****"
 exit 0
